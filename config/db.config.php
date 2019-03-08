585a0b5d105385bff86078e8fa4aef9927304f7d
Xiangrui2019 <xiangruikong2019@outlook.com>
2019-03-08 21:39:51 +0800

    Change Dev Env To Linux For LAMPP

--- config/db.config.php
+++ config/db.config.php
@@ -12,10 +12,10 @@ if (defined('SAE_APPNAME')) {
     $GLOBALS['config']['db']['db_name'] = SAE_MYSQL_DB;
 } else {
     // 如果使用本地环境, 那么, 使用本地的数据库配置
-    // 此套数据库配置为PHPStudy设计, 如果需要使用WAMP MAMP XAMPP 请自行更改
+    // 此套数据库配置为XAMPP设计, 如果需要使用WAMP MAMP Phpstudy 请自行更改
     $GLOBALS['config']['db']['db_host'] = 'localhost'; // 数据库地址
     $GLOBALS['config']['db']['db_port'] = 3306; // 数据库端口
     $GLOBALS['config']['db']['db_user'] = 'root'; // 数据库用户名
-    $GLOBALS['config']['db']['db_password'] = 'root'; // 数据库密码
+    $GLOBALS['config']['db']['db_password'] = ''; // 数据库密码
     $GLOBALS['config']['db']['db_name'] = 'publishdb'; // 数据库[需要预先创建, 不可使用mysql默认的几个数据库]
 }
